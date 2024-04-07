1d03974d2a038fa4e2f6aeaf862404de30aaf59a
Livsy90 <artmir@live.ru>
2024-04-07 10:31:20 +0400

    Initial Commit

---
+++ ProjectSUICoordinator/ProjectSUICoordinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectSUICoordinatorApp.swift
+//  ProjectSUICoordinator
+//
+//  Created by Livsy on 07.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct ProjectSUICoordinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
