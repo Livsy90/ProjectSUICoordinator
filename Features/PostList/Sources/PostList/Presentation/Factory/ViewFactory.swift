e756aa6cc3cbf6ecdaa19162ac97c328ee595153
Livsy90 <artmir@live.ru>
2024-04-27 15:50:04 +0400

    CodeStyle

--- Features/PostList/Sources/PostList/Presentation/Factory/ViewFactory.swift
+++ Features/PostList/Sources/PostList/Presentation/Factory/ViewFactory.swift
@@ -21,14 +21,17 @@ struct ViewFactory {
         switch kind {
         case .postList:
             PostListView(viewModel: PostListViewModel())
+            
         case .postDetails(let model):
             PostDetailsView(viewModel: PostDetailViewModel(content: model))
+            
         case .share(let model):
             Text(model)
                 .padding()
                 .background(.purple)
                 .foregroundStyle(.white)
                 .font(.largeTitle)
+            
         case .menu(let model):
             Text(model)
                 .padding()
